f9031c0e50b226777679b7d564e7f00af3727897
Yozu Liang <yozukitty@gmail.com>
2020-04-07 13:37:19 -0700

    Updates

---
+++ component---src-pages-page-2-js-179a44076c439432f0bb.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{p5nM:function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),o=a.n(n),l=a("Wbzz"),c=a("Zttt"),r=a("vrFN");t.default=function(){return o.a.createElement(c.a,null,o.a.createElement(r.a,{title:"Page two"}),o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(l.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-179a44076c439432f0bb.js.map
